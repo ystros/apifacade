00f740509073685bf8fb39ecbff21a707affe7a0
Sonjaya Tandon <sonjayatandon@gmail.com>
2017-11-12 19:09:15 -0800

    travis should fail CI`

--- test/unit/lib/route-to-service.js
+++ test/unit/lib/route-to-service.js
@@ -1,9 +1,11 @@
 var routeToService = require('../../../src/lib/route-to-service.js');
+const assert = require('assert');
 
 describe('lib/route-to-service', () => { 
   describe('#getServiceURL', () => {
     context('discovery method: docker-compose', () => {
       it('should not append or prepend anything to the service name', () => { 
+        assert.equal(true, false);
       });
     });
   });
